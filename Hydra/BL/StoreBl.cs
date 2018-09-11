8952c24d941f828aea0c573aea03bfa4d7f57273
Nir <GuyCo@LAPTOP-GGSEK3R5>
2018-09-11 12:01:49 +0300

    added store controller and its view

---
+++ Hydra/BL/StoreBl.cs
@@ -0,0 +1,26 @@
+﻿using Hydra.DAL;
+using Hydra.Data;
+using Hydra.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hydra.BL
+{
+    public class StoreBl
+    {
+        private readonly StoreDataAccess _storeDataAccess;
+
+        public StoreBl(HydraContext hydraContext)
+        {
+            _storeDataAccess = new StoreDataAccess(hydraContext);
+        }
+
+        public List<Store> GetAllStores()
+        {
+            return _storeDataAccess.GetAllStores();
+        }
+
+    }
+}
---
+++ Hydra/Controllers/StoreController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hydra.BL;
+using Hydra.DAL;
+using Hydra.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hydra.Controllers
+{
+    public class StoreController : Controller
+    {
+        private readonly StoreBl _storeBl;
+
+        public StoreController(HydraContext hydraContext)
+        {
+            _storeBl = new StoreBl(hydraContext);
+        }
+
+
+        // GET: Store
+        public ActionResult Index()
+        {
+            return View(_storeBl.GetAllStores());
+        }
+
+        // GET: Store/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Store/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Store/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Store/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Store/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Store/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Store/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
