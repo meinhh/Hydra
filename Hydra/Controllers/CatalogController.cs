e718881fbde3c5de503d305129e4c8f6a2db1382
LAPTOP-1GD0DSQ7\gal <galhen400@gmail.com>
2018-10-13 11:32:38 +0300

    Catalog addition + added links to it

---
+++ Hydra/Controllers/CatalogController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hydra.BL;
+using Hydra.DAL;
+using Hydra.Data;
+using Hydra.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hydra.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly HydraContext _hydraContext;
+        private readonly ProductBl _productBl;
+
+        public CatalogController(HydraContext hydraContext)
+        {
+            _hydraContext = hydraContext;
+            _productBl = new ProductBl(hydraContext);
+        }
+
+        // GET: Catalog
+        public ActionResult Index()
+        {
+            return View(_productBl.GetAllProducts());
+        }
+
+        // GET: Catalog/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Catalog/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Catalog/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Catalog/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Catalog/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Catalog/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Catalog/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
