133efdd03a80801b7095e3a37963716b93eae056
Meirav Shenhar <meinhh@gmail.com>
2018-11-09 11:00:40 +0200

    remove unused code from map

--- Hydra/wwwroot/js/map.js
+++ Hydra/wwwroot/js/map.js
@@ -1,4 +1,4 @@
-﻿let map, dataLayer, infobox, searchManager;
+﻿let map;
 const distanceUnits = 'km',
     dataSourceUrl = 'https://spatial.virtualearth.net/REST/v1/data/515d38d4d4e348d9a61c615f59704174/CoffeeShops/CoffeeShop';
 
@@ -7,24 +7,11 @@ function getMap() {
         zoom: 18,
         center: new Microsoft.Maps.Location(32.078863, 34.794393)
     });
-    dataLayer = new Microsoft.Maps.Layer();
-    Microsoft.Maps.Events.addHandler(dataLayer, 'click', (e => displayInfobox(e.primitive)));
-    map.layers.insert(dataLayer);
-    infobox = new Microsoft.Maps.Infobox(new Microsoft.Maps.Location(0, 0), {
-        visible: false,
-        offset: new Microsoft.Maps.Point(0, 20),
-        height: 170,
-        width: 230
-    });
-    infobox.setMap(map);
-    Microsoft.Maps.loadModule(['Microsoft.Maps.Search', 'Microsoft.Maps.SpatialDataService', 
-        'Microsoft.Maps.SpatialMath'], () => searchManager = new Microsoft.Maps.Search.SearchManager(map));
     performSearch();
     $('#search-btn').click(performSearch);
     $('#searchBox').keypress((e) => {
-        if (e.which == 13) {
+        if (e.which == 13)
             performSearch();
-        }
     });
 }
 
@@ -80,7 +67,6 @@ function showStoresOnMap(stores) {
     const locations = stores.map(store => {
         const loc = new Microsoft.Maps.Location(store.latitude, store.lontitude);
         const pin = new Microsoft.Maps.Pushpin(loc);
-        //Microsoft.Maps.Events.addHandler(pin, 'click', () => window.location = `/Catalog/ByStore/${store.id}` );
         Microsoft.Maps.Events.addHandler(pin, 'click', () => window.location = `/Store/ById/${store.id}`);
         map.entities.push(pin);
 
